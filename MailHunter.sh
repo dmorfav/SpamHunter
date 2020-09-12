28ece0f393e6caa1c2b0055b6c5cb42bb6dfac5a
dmorfav <dmorfav@gmail.com>
2020-09-11 22:27:21 -0400

    add update a element of list

--- MailHunter.sh
+++ MailHunter.sh
@@ -6,6 +6,15 @@
 #Global variables
 FILE="/home/dmorfav/postfix_reject_sender"
 
+#This method list the file with all blocked domains and emails
+function list() {
+  if [ -f $FILE ]; then
+      cat $FILE
+  else
+    echo "El fichero $FILE no existe"
+  fi
+}
+
 #This function is used for add a new domain or email to blocked list
 #If pass a parameter then execute the process automatic else ask to user
 function add() {
@@ -20,20 +29,25 @@ function add() {
   echo "Agregado correctamente"
 }
 
+function update() {
+  echo "Listado de elementos"
+  echo "####################"
+  list
+  echo ""
+    # Text to search
+  read -p "Introduce el elemento que desea remplazar: " search
+    # Text to replace
+  read -p "Introduce el nuevo valor: " replace
+
+  if [[ $search != "" && $replace != "" ]]; then
+    sed -i "s/$search/$replace/" $FILE
+  fi
+}
 
 #This function is used for create the file if not exist
 function init() {
   if [ ! -f $FILE ]; then
       touch $FILE
-  fi
-}
-
-#This method list the file with all blocked domains and emails
-function list() {
-  if [ ! -f $FILE ]; then
-      cat $FILE
-  else
-    echo "El fichero $FILE no existe"
   fi
 }
 
@@ -47,12 +61,16 @@ function execute() {
       'l' )
         list
         ;;
+      'a' )
+        update
+        ;;
     esac
 }
 
 case $1 in
   'a')
-    echo "update"
+  echo "Actualizando un dominio o email bloqueado"
+  echo "#######################################"
     execute $1 $2
   ;;
   'd')
